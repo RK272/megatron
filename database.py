db664c42ca5c1eddd8e4a4504bf89633f492baa4
RK272 <rk7918495@gmail.com>
2023-12-09 11:12:40 +0530

    first

--- database.py
+++ database.py
@@ -1,11 +1,11 @@
 from pymongo import MongoClient
 
 client = MongoClient(
-    "mongodb+srv://megatronLog:hr2SC3S5Wcx2uip3@cluster0.3oipc.mongodb.net/megatronUser?retryWrites=true&w=majority")
+    "mongodb+srv://rithin:076ecHwHg60yETd9@cluster0.ctrleyy.mongodb.net/?retryWrites=true&w=majority")
 db = client.megatronUser
 
 client_admin = MongoClient(
-    "mongodb+srv://megatronUser:megatronPass@dashboard-cluster.fbim5.mongodb.net/megatronDB?retryWrites=true&w=majority")
+    "mongodb+srv://rithin:076ecHwHg60yETd9@cluster0.ctrleyy.mongodb.net/?retryWrites=true&w=majority")
 db_admin = client_admin.megatronDB
 
 class mongo_connection:
